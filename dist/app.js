41e4f572ec26c2abf84862ef02701e6d0323b040
david11yf29 <david11yf29@gmail.com>
2020-09-03 08:59:06 +0800

    updated

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,18 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        console.log('TemplateElement', this.templateElement);
+        const importNode = document.importNode(this.templateElement.content, true);
+        console.log('ImportNode', importNode);
+        this.element = importNode.firstElementChild;
+        console.log('FirstElement for importNode', this.element);
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
 // Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    console.log('TemplateElement', this.templateElement);
+
+    const importNode = document.importNode(this.templateElement.content, true)
+    console.log('ImportNode', importNode);
+
+    this.element = importNode.firstElementChild as HTMLFormElement;
+    console.log('FirstElement for importNode', this.element);
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
+
