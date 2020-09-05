e731d2191ab1f9e5e5fe91957977381b79470d3b
david11yf29 <david11yf29@gmail.com>
2020-09-05 23:29:42 +0800

    updated

--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -1,34 +1,32 @@
-namespace App {
-  // Component Class
-  export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
-    templateElement: HTMLTemplateElement;
-    hostElement: T;
-    element: U;
+// Component Class
+export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
 
-    constructor(
-      templateId: string,
-      hostElementId: string,
-      insertAtStart: boolean,
-      newElementId?: string
-    ) {
-      this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
-      this.hostElement = document.getElementById(hostElementId)! as T;
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
 
-      const importNode = document.importNode(this.templateElement.content, true)
-      this.element = importNode.firstElementChild as U;
+    const importNode = document.importNode(this.templateElement.content, true)
+    this.element = importNode.firstElementChild as U;
 
-      if (newElementId) {
-        this.element.id = newElementId;
-      }
-
-      this.attach(insertAtStart);
+    if (newElementId) {
+      this.element.id = newElementId;
     }
 
-    private attach(insertAtBeginning: boolean) {
-      this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);
-    }
-
-    abstract configure(): void;
-    abstract renderContent() : void;
+    this.attach(insertAtStart);
   }
+
+  private attach(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);
+  }
+
+  abstract configure(): void;
+  abstract renderContent() : void;
 }
